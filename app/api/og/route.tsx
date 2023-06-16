00a8d7caea22348dea3fdd9070d9a8a6186caa7b
Impaler3 <97364729+ajemba@users.noreply.github.com>
2023-06-16 11:26:24 -0400

    final

---
+++ app/api/og/route.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+ 
+function Avatar() {
+  return <Image src="https://ekrpt.com/images/opengraph-image.png" alt="og" width="64" height="64" />
+}
+
+export default Avatar
